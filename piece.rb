dfce2d6ebe7c4bfcec7b70b399b4408b5bf8abd3
Iliyan Bobev <bobev18@gmail.com>
2013-02-23 21:57:51 +0200

    bit of cleanup

--- piece.rb
+++ piece.rb
@@ -1,11 +1,5 @@
 #piece.rb
 
-# def p2s(x,y)
-#     # returns square designation given coordinates
-#     # allows for invalid conversion, which need to be filtered out at the end
-#     chr(96+x)+str(y)
-# end
-
 class Piece
     attr_accessor :color, :type, :square, :x, :y
     CAPTURE_SIGN = 'x'
@@ -49,24 +43,8 @@ class Piece
     end
 
     def expand(board_state)
-        # list possible moves based on board setup only(board_state is dict representation of the board not the board object)
-        ########  result is returned as notation integrated in the command representation i.e. tuple(comm,sq,notation)
-
-        #at = self.class.send(:define_method, { |dx,dy| board_state[p2s(dx,dy)] })
-
-
+        # list possible moves based on board setup only (board_state is dict representation of the board not the board object)
         result = []
-        #if not self.onboard:
-        #    return result
-
-        # result representation in triplets of (a,sq,note) where
-        #   action= one of the following desingation m=move t=take e=en passant c=castle p=promo +=take+promo
-        #                             sq = square is param for the action
-        #   from = current position of the piece
-        #   to   = the position of the piece after the move completion
-        #   note = notation*
-        #     *   Notation is close to Algebraic_chess_notation http://en.wikipedia.org/wiki/Algebraic_chess_notation but
-        #         the full complience will need additional steps to add disambiguation, checks and mate
 
         #white pawn:
         if @color == 'w' and @type == 'p'
@@ -96,11 +74,6 @@ class Piece
                 result += ['R','N','B','Q'].collect { |promo| {action: '+', from: @square, to: p2s(displacement), notation: @square[0] + CAPTURE_SIGN + p2s(displacement) + promo} }
             end
             if @y==5 and not board_state[p2s(displacement)] and board_state[p2s(+1,0)]=='bp' then result << {action: 'e', from: @square, to: p2s(displacement), notation: @square[0] + CAPTURE_SIGN + p2s(displacement)} end
-
-
-            # if @y>=2 and @y<7 and board_state[p2s(displacement)] and board_state[p2s(displacement)][0]=='b' then result.append(('t',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
-            # if @y==7 and board_state[p2s(displacement)] and board_state[p2s(displacement)][0]=='b' end result.extend([ ('+',p2s(displacement),p2s(displacement)+z_) for z_ in ['R','N','B','Q']]) end
-            # if @y==5 and board_state[p2s(displacement)]=='  ' and board_state[p2s((@x+1,@y)])=='bp' then result.append(('e',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
         end
 
         if @color == 'b' and @type == 'p'
@@ -129,22 +102,6 @@ class Piece
                 result += ['R','N','B','Q'].collect { |promo| {action: '+', from: @square, to: p2s(displacement), notation: @square[0] + CAPTURE_SIGN + p2s(displacement) + promo} }
             end
             if @y==4 and not board_state[p2s(displacement)] and board_state[p2s(@x+1,@y)]=='bp' then result << {action: 'e', from: @square, to: p2s(displacement), notation: @square[0] + CAPTURE_SIGN + p2s(displacement)} end
-
-
-            # displacement = @x,@y-2
-            # if @y==7 and board_state[p2s((@x,6)])=='  ' and board_state[p2s((@x,5)])=='  ' then result.append(('m',p2s(displacement),p2s(displacement))) end
-            # displacement = @x,@y-1
-            # if @y<=7 and @y>2 and board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),p2s(displacement))) end
-            # if @y==2 and board_state[p2s(displacement)]=='  ' then result.extend([ ('m',p2s(displacement),p2s(displacement)+z_) for z_ in ['R','N','B','Q']]) end
-
-            # displacement = @x-1,@y-1
-            # if @y>=7 and @y>2 and board_state[p2s(displacement)][0]=='w' then result.append(('t',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
-            # if @y==2 and board_state[p2s(displacement)][0]=='w' then result.extend([ ('+',p2s(displacement),p2s(displacement)+z_) for z_ in ['R','N','B','Q']]) end
-            # if @y==5 and board_state[p2s(displacement)]=='  ' and board_state[p2s((@x-1,@y)])=='wp' then result.append(('e',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
-            # displacement = @x+1,@y-1
-            # if @y>=7 and @y>2 and board_state[p2s(displacement)][0]=='w' then result.append(('t',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
-            # if @y==2 and board_state[p2s(displacement)][0]=='w' then result.extend([ ('+',p2s(displacement),p2s(displacement)+z_) for z_ in ['R','N','B','Q']]) end
-            # if @y==4 and board_state[p2s(displacement)]=='  ' and board_state[p2s((@x+1,@y)])=='wp' then result.append(('e',p2s(displacement),self.sq[0]+CAPTURE_SIGN+p2s(displacement))) end
         end
 
         if @type == 'n'
@@ -155,35 +112,6 @@ class Piece
                     if board_state[p2s(x,y)][0] != @color then result << {action: 't', from: @square, to: p2s(x,y), notation: 'Nx'+p2s(x,y)} end
                 end
             end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement =@x+2,@y-1
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement = @x+2,@y+1
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement = @x+1,@y+2
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement = @x-1,@y-2
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement =@x-2,@y-1
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement =@x-2,@y+1
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
-            # displacement =
-            # if board_state[p2s(displacement)]=='  ' then result.append(('m',p2s(displacement),'N'+p2s(displacement))) end
-            # displacement =@x-1,@y+2
-            # if board_state[p2s(displacement)]!='  ' and board_state[p2s(displacement)][0]!=@color: result.append(('t',p2s(displacement),'Nx'+p2s(displacement))) end
         end
 
         if ['q','k','b','r'].include? @type
@@ -203,22 +131,6 @@ class Piece
                 {name: 'W' , flag: false, types: ['q','r','k'], displacement: [-1, 0 ]},
                 {name: 'NW', flag: false, types: ['q','b','k'], displacement: [-1, 1 ]},
                 {name: 'N' , flag: false, types: ['q','r','k'], displacement: [ 0, 1 ]},
-
-                # {name: 'E' , flag: false, types: ['q','r','k'], displacement: lambda { |x,y,j| x+j, y   }},
-                # {name: 'SE', flag: false, types: ['q','b','k'], displacement: lambda { |x,y,j| x+j, y-j }},
-                # {name: 'S' , flag: false, types: ['q','r','k'], displacement: lambda { |x,y,j| x  , y-j }},
-                # {name: 'SW', flag: false, types: ['q','b','k'], displacement: lambda { |x,y,j| x-j, y-j }},
-                # {name: 'W' , flag: false, types: ['q','r','k'], displacement: lambda { |x,y,j| x-j, y   }},
-                # {name: 'NW', flag: false, types: ['q','b','k'], displacement: lambda { |x,y,j| x-j, y+j }},
-                # {name: 'N' , flag: false, types: ['q','r','k'], displacement: lambda { |x,y,j| x  , y+j }},
-
-                # {name: 'E', flag: false, disp_x: :+, disp_y: nil},
-                # {name: 'SE', flag: false, disp_x: :+, disp_y: :-},
-                # {name: 'S', flag: false, disp_x: nil, disp_y: :-},
-                # {name: 'SW', flag: false, disp_x: :-, disp_y: :-},
-                # {name: 'W', flag: false, disp_x: :-, disp_y: nil},
-                # {name: 'NW', flag: false, disp_x: :-, disp_y: :+},
-                # {name: 'N', flag: false, disp_x: nil, disp_y: :+},
             ]
             1.upto maxrange do |i|
                 directions.each do |direction|
@@ -236,79 +148,6 @@ class Piece
                         end
                     end
                 end
-
-
-                # displacement = @x+i,@y+i
-                # if not NE and @type in ['q','b','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         NE = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x+i,@y-i
-                # if not SE and @type in ['q','b','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         SE = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x-i,@y-i
-                # if not SW and @type in ['q','b','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         SW = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x-i,@y+i
-                # if not NW and @type in ['q','b','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         NW = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x,@y+i
-                # if not N and @type in ['q','r','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         N = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x+i,@y
-                # if not E and @type in ['q','r','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         E = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x,@y-i
-                # if not S and @type in ['q','r','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         S = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
-
-                # displacement = @x-i,@y
-                # if not W and @type in ['q','r','k']:
-                #     if board_state[p2s(displacement)]=='  ':
-                #         result.append(('m',p2s(displacement),@type.upper()+p2s(displacement)))
-                #     else:
-                #         W = True
-                #         if board_state[p2s(displacement)][0]!=@color:
-                #             result.append(('t',p2s(displacement),@type.upper()+'x'+p2s(displacement)))
             end
         end
 
@@ -322,7 +161,7 @@ class Piece
                 result << {action: 'c', from: @square, to: p2s(-2,0), notation: 'O-O-O'}
             end
         end
-
+        puts "results #{result}"
         result.select { |expansion| expansion[:to] != nil } #excludes out of the board expansions
     end
 end
--- piece_spec.rb
+++ piece_spec.rb
@@ -63,13 +63,6 @@ describe Piece do
             # |  |  |  |  |  |  |  |  |
             # |wr|wn|wb|wq|wk|wb|  |wr|
             # """
-
-
-
-            #for sq in ['b5','c8','d7','c1','a8','b7','e5','d1','e8','e1']:
-            #    p = self.zboard.piece_by_sq(sq)
-            #    print(p,p.expand(self.zboard.board))
-
             #white bishop at b5
             piece = Piece.new 'w','b', 'b5'
             piece.expand(TEST2_BOARD).collect { |move| move[:notation] }.sort.should eq ['Ba4', 'Ba6', 'Bc6','Bxd7','Bc4','Bd3','Be2'].sort
@@ -104,110 +97,5 @@ describe Piece do
             piece = Piece.new 'w','k', 'e1'
             piece.expand(TEST2_BOARD).collect { |move| move[:notation] }.sort.should eq ['Kd2', 'Kf2', 'Ke2'].sort
         end
-
     end
-
-    # test_simple_expand_white_pawns(self):
- #        self.zboard.initialset()
- #        rezlist = []
- #        for p in self.zboard.whites:
- #            if p.type=='p':
- #                rezlist.append((repr(p),p.expand(self.zboard.board)))
-
- #        goallist = [( 'wp@a2' , ['a4', 'a3'] ),
- #                    ( 'wp@b2' , ['b4', 'b3'] ),
- #                    ( 'wp@c2' , ['c4', 'c3'] ),
- #                    ( 'wp@d2' , ['d4', 'd3'] ),
- #                    ( 'wp@e2' , ['e4', 'e3'] ),
- #                    ( 'wp@f2' , ['f4', 'f3'] ),
- #                    ( 'wp@g2' , ['g4', 'g3'] ),
- #                    ( 'wp@h2' , ['h4', 'h3'] ),]
- #        #print('goal:',goallist)
- #        #print('rez :',rezlist)
- #        self.assertEqual(goallist,rezlist)
-
-    # def test_expand_pawns_knights(self):
- #        self.zboard.piecefy({'h8': '  ', 'h2': '  ', 'h3': '  ', 'h1': 'wr', 'h6': '  ', 'h7': 'wp', 'h4': '  ', 'h5': '  ', 'd8': 'bq', 'a8': 'br', 'd6': '  ', 'd7': 'bp', 'd4': '  ', 'd5': '  ', 'd2': 'wp', 'd3': '  ', 'd1': 'wq', 'g7': 'bp', 'g6': '  ', 'g5': 'wp', 'g4': '  ', 'g3': '  ', 'g2': '  ', 'g1': '  ', 'g8': 'bn', 'c8': 'bb', 'c3': 'bn', 'c2': 'wp', 'c1': 'wb', 'c7': 'bp', 'c6': '  ', 'c5': '  ', 'c4': '  ', 'f1': 'wb', 'f2': 'wp', 'f3': '  ', 'f4': '  ', 'f5': 'bp', 'f6': '  ', 'f7': '  ', 'f8': 'bb', 'b4': '  ', 'b5': '  ', 'b6': '  ', 'b7': 'bp', 'b1': 'wn', 'b2': 'wp', 'b3': '  ', 'b8': '  ', 'a1': 'wr', 'a3': '  ', 'a2': 'wp', 'a5': '  ', 'e8': 'bk', 'a7': 'bp', 'a6': '  ', 'e5': '  ', 'e4': 'wn', 'e7': 'bp', 'e6': '  ', 'e1': 'wk', 'e3': '  ', 'e2': 'wp', 'a4': '  '})
-
- #        #pawn capture right
- #        self.assertEqual(set(['b4', 'b3', 'bxc3']),set(self.zboard.piece_by_sq('b2').expand(self.zboard.board)))
- #        #pawn capture right
- #        self.assertEqual(set(['d4', 'd3', 'dxc3']),set(self.zboard.piece_by_sq('d2').expand(self.zboard.board)))
- #        #blocked pawn
- #        self.assertEqual([],self.zboard.piece_by_sq('c2').expand(self.zboard.board))
- #        self.assertEqual(['e3'],self.zboard.piece_by_sq('e2').expand(self.zboard.board))
- #        #en passant no lastmove match, then with lastmovematch
- #        self.assertEqual(['g6'],self.zboard.piece_by_sq('g5').expand(self.zboard.board,self.zboard.lastmove))
- #        self.zboard.lastmove = 'f5'
- #        self.assertEqual(set(['g6','gxf6']),set(self.zboard.piece_by_sq('g5').expand(self.zboard.board,self.zboard.lastmove)))
- #        #promote pawn h7
- #        self.assertEqual(set(['h8R','h8N','h8B','h8Q','hxg8R','hxg8N','hxg8B','hxg8Q']),set(self.zboard.piece_by_sq('h7').expand(self.zboard.board,self.zboard.lastmove)))
-
- #        #knight at e4
- #        self.assertEqual(set(['Nf6','Ng3','Nxc3','Nc5','Nd6']),set(self.zboard.piece_by_sq('e4').expand(self.zboard.board)))
- #        #knight at c3
- #        self.assertEqual(set(['Nd5','Nxe4','Nxe2','Nxd1','Nxb1','Nxa2','Na4','Nb5']),set(self.zboard.piece_by_sq('c3').expand(self.zboard.board)))
-
- #    def test_add(self):
- #        self.zboard.piecefy({'h8': '  ', 'h2': '  ', 'h3': '  ', 'h1': 'wr', 'h6': '  ', 'h7': 'wp', 'h4': '  ', 'h5': '  ', 'd8': 'bq', 'a8': 'br', 'd6': '  ', 'd7': 'bp', 'd4': '  ', 'd5': '  ', 'd2': 'wp', 'd3': '  ', 'd1': 'wq', 'g7': 'bp', 'g6': '  ', 'g5': 'wp', 'g4': '  ', 'g3': '  ', 'g2': '  ', 'g1': '  ', 'g8': 'bn', 'c8': 'bb', 'c3': 'bn', 'c2': 'wp', 'c1': 'wb', 'c7': 'bp', 'c6': '  ', 'c5': '  ', 'c4': '  ', 'f1': 'wb', 'f2': 'wp', 'f3': '  ', 'f4': '  ', 'f5': 'bp', 'f6': '  ', 'f7': '  ', 'f8': 'bb', 'b4': '  ', 'b5': '  ', 'b6': '  ', 'b7': 'bp', 'b1': 'wn', 'b2': 'wp', 'b3': '  ', 'b8': '  ', 'a1': 'wr', 'a3': '  ', 'a2': 'wp', 'a5': '  ', 'e8': 'bk', 'a7': 'bp', 'a6': '  ', 'e5': '  ', 'e4': 'wn', 'e7': 'bp', 'e6': '  ', 'e1': 'wk', 'e3': '  ', 'e2': 'wp', 'a4': '  '})
- #        for p in self.zboard.fullset():
- #            if p.type == 'p':
- #                self.zboard.take(p)
-
- #        self.zboard.add('w','b','b5')
- #        self.zboard.add('bq@e5')
- #        self.zboard.add('bb@d7')
- #        self.zboard.add('wr@b7')
- #        #self.zboard.boardify()
- #        #print(self.zboard.board)
- #        self.assertEqual('wb@b5',repr(self.zboard.piece_by_sq('b5')))
- #        self.assertEqual('bq@e5',repr(self.zboard.piece_by_sq('e5')))
- #        self.assertEqual('bb@d7',repr(self.zboard.piece_by_sq('d7')))
- #        self.assertEqual('wr@b7',repr(self.zboard.piece_by_sq('b7')))
-
- #    def test_expand_bishop_rook_queen_king(self):
- #        self.zboard.piecefy({'h8': '  ', 'h2': '  ', 'h3': '  ', 'h1': 'wr', 'h6': '  ', 'h7': '  ', 'h4': '  ', 'h5': '  ', 'd8': 'bq', 'a8': 'br', 'd6': '  ', 'd7': 'bb', 'd4': '  ', 'd5': '  ', 'd2': '  ', 'd3': '  ', 'd1': 'wq', 'g7': '  ', 'g6': '  ', 'g5': '  ', 'g4': '  ', 'g3': '  ', 'g2': '  ', 'g1': '  ', 'g8': 'bn', 'c8': 'bb', 'c3': 'bn', 'c2': '  ', 'c1': 'wb', 'c7': '  ', 'c6': '  ', 'c5': '  ', 'c4': '  ', 'f1': 'wb', 'f2': '  ', 'f3': '  ', 'f4': '  ', 'f5': '  ', 'f6': '  ', 'f7': '  ', 'f8': 'bb', 'b4': '  ', 'b5': 'wb', 'b6': '  ', 'b7': 'wr', 'b1': 'wn', 'b2': '  ', 'b3': '  ', 'b8': '  ', 'a1': 'wr', 'a3': '  ', 'a2': '  ', 'a5': '  ', 'e8': 'bk', 'a7': '  ', 'a6': '  ', 'e5': 'bq', 'e4': 'wn', 'e7': '  ', 'e6': '  ', 'e1': 'wk', 'e3': '  ', 'e2': '  ', 'a4': '  '})
- #        #print('\n'+self.zboard.show())
-
- #        """
- #        |br|  |bb|bq|bk|bb|bn|  |
- #        |  |wr|  |bb|  |  |  |  |
- #        |  |  |  |  |  |  |  |  |
- #        |  |wb|  |  |bq|  |  |  |
- #        |  |  |  |  |wn|  |  |  |
- #        |  |  |bn|  |  |  |  |  |
- #        |  |  |  |  |  |  |  |  |
- #        |wr|wn|wb|wq|wk|wb|  |wr|
- #        """
-
- #        #for sq in ['b5','c8','d7','c1','a8','b7','e5','d1','e8','e1']:
- #        #    p = self.zboard.piece_by_sq(sq)
- #        #    print(p,p.expand(self.zboard.board))
-
- #        #white bishop at b5
- #        self.assertEqual(set(['Ba4', 'Ba6', 'Bc6','Bxd7','Bc4','Bd3','Be2']),set(self.zboard.piece_by_sq('b5').expand(self.zboard.board)))
- #        #bishop at c8
- #        self.assertEqual(['Bxb7'],self.zboard.piece_by_sq('c8').expand(self.zboard.board))
- #        #bishop at d7
- #        self.assertEqual(set(['Bc6', 'Bxb5', 'Be6','Bf5','Bg4','Bh3']),set(self.zboard.piece_by_sq('d7').expand(self.zboard.board)))
- #        #bishop at c1
- #        self.assertEqual(set(['Ba3', 'Bb2', 'Bd2','Be3','Bf4','Bg5','Bh6']),set(self.zboard.piece_by_sq('c1').expand(self.zboard.board)))
-
- #        #rook at a8
- #        self.assertEqual(set(['Rxa1', 'Ra2', 'Ra3','Ra4','Ra5','Ra6','Ra7','Rb8']),set(self.zboard.piece_by_sq('a8').expand(self.zboard.board)))
- #        #rook at b7
- #        self.assertEqual(set(['Rb6', 'Rc7', 'Rxd7','Rb8','Ra7']),set(self.zboard.piece_by_sq('b7').expand(self.zboard.board)))
-
- #        #queen at e5
- #        self.assertEqual(set(['Qe7', 'Qe6', 'Qf6','Qg7','Qh8','Qf5', 'Qg5', 'Qh5','Qf4','Qg3','Qh2', 'Qxe4', 'Qd4','Qd5','Qc5','Qxb5', 'Qd6','Qc7','Qb8',]),set(self.zboard.piece_by_sq('e5').expand(self.zboard.board)))
- #        #queen at d1
- #        self.assertEqual(set(['Qc2', 'Qb3', 'Qa4','Qd2','Qd3','Qd4', 'Qd5', 'Qd6','Qxd7','Qe2','Qf3', 'Qg4', 'Qh5']),set(self.zboard.piece_by_sq('d1').expand(self.zboard.board)))
-
- #        #king at e8
- #        self.assertEqual(set(['Ke7', 'Kf7']),set(self.zboard.piece_by_sq('e8').expand(self.zboard.board)))
- #        #king at e1
- #        self.assertEqual(set(['Kd2', 'Kf2', 'Ke2']),set(self.zboard.piece_by_sq('e1').expand(self.zboard.board)))
-
-
 end
-
